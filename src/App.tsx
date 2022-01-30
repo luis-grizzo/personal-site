63bdf4202c684b32a39432519f3b88099d6964a4
Luis Grizzo <luisoggrizzo@gmail.com>
2022-01-30 03:48:38 -0300

    refactor: remove App file and config index project file

--- src/App.tsx
+++
@@ -1,7 +0,0 @@
-function App(): React.ReactElement {
-  return (
-    <h1>Olá React!</h1>
-  )
-}
-
-export default App
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import App from './App'
+
+import Routes from './routes'
+import GlobalStyle from './styles/global'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <GlobalStyle/>
+    <Routes/>
   </React.StrictMode>,
   document.getElementById('root')
 )
