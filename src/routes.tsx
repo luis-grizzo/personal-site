682162a6609397a6b27d0fc825260d37bf017830
Luis Grizzo <luisoggrizzo@gmail.com>
2022-01-30 03:49:05 -0300

    feat(routes): add project router structure

---
+++ src/routes.tsx
@@ -0,0 +1,25 @@
+import { lazy, Suspense } from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+import Loading from './components/Loading'
+
+const MainLayout = lazy(async () => await import('./layouts/MainLayout'))
+const Home = lazy(async () => await import('./pages/Home'))
+const Contact = lazy(async () => await import('./pages/Contact'))
+
+const Router = (): React.ReactElement => {
+  return (
+    <BrowserRouter>
+      <Suspense fallback={<Loading />}>
+        <MainLayout>
+          <Routes>
+            <Route index element={<Home />} />
+            <Route path='/contact' element={<Contact />} />
+          </Routes>
+        </MainLayout>
+      </Suspense>
+    </BrowserRouter>
+  )
+}
+
+export default Router
