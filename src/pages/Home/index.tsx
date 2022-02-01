e983514ee877483ed07ad94454933a266e4436d9
Luís Grizzo <luis.grizzo@liveonsolutions.com>
2022-02-01 19:04:27 -0300

    feat(Home): add new button and configure styles

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -15,11 +15,16 @@ const Home = (): React.ReactElement => {
         <strong className='wt__highlight'>UI-Designer</strong>
       </h1>
       <p className="w__description">
-        Me chamo <a href='https://www.linkedin.com/in/lu%C3%ADs-ot%C3%A1vio-gaido-grizzo-2a957a1b2/' target='_blank' className='wd__link'>Luís Grizzo</a> e este é meu site, aqui você encontra todos meus links para contato, obrigado pela visita!
+        Me chamo <a href='https://www.linkedin.com/in/lu%C3%ADs-ot%C3%A1vio-gaido-grizzo-2a957a1b2/' target='_blank' className='wd__link'>Luís Grizzo</a> e este é meu site! Aqui você pode conhecer um pouco sobre mim e encontrar todos os meus links para contato, obrigado pela visita!
       </p>
-      <Button onClick={() => navigate('/contact')}>
-        Entre em contato
-      </Button>
+      <div className="w__button-wrapper">
+        <Button variant='secondary' onClick={() => navigate('/about')}>
+          Sobre mim
+        </Button>
+        <Button onClick={() => navigate('/contact')}>
+          Entre em contato
+        </Button>
+      </div>
     </S.Wrapper>
   )
 }
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -1,35 +1,45 @@
-import styled from 'styled-components'
-
-import { theme } from '../../styles/theme'
+import styled, { css } from 'styled-components'
 
 export const Wrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: flex-start;
-  gap: 5rem;
+  ${({ theme }) => css`
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    gap: 5rem;
 
-  .w__title {
-    font-size: 7.2rem;
-    font-weight: 300;
+    width: 100%;
 
-    .wt__highlight {
-      font-weight: 700;
-    }
-  }
+    .w__title {
+      font-size: 7.2rem;
+      font-weight: 300;
 
-  .w__description {
-    color: ${theme.colors.text};
-
-    .wd__link {
-      font-weight: 700;
-      color: ${theme.colors.primary};
-
-      will-change: color;
-      transition: ${theme.transitions.default};
-
-      &:hover {
-        color: ${theme.colors.primaryHover}
+      .wt__highlight {
+        font-weight: 700;
       }
     }
-  }
+
+    .w__description {
+      font-size: 2rem;
+      color: ${theme.colors.text};
+
+      .wd__link {
+        font-weight: 700;
+        color: ${theme.colors.primary};
+
+        will-change: color;
+        transition: ${theme.transitions.default};
+
+        &:hover {
+          color: ${theme.colors.primaryHover}
+        }
+      }
+    }
+
+    .w__button-wrapper {
+      display: flex;
+      align-items: center;
+      flex-wrap: wrap;
+      gap: 2rem;
+    }
+  `}
 `
