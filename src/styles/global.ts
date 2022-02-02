a33d334df8d2f1dadcd216f2c9f85ceb6d26e33d
Luís Grizzo <luis.grizzo@liveonsolutions.com>
2022-02-02 00:15:34 -0300

    feat(GlobalStyles): set new reset styles and add GlobalStyles in theme provider

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,83 +1,102 @@
-import { createGlobalStyle } from 'styled-components'
+import { createGlobalStyle, css } from 'styled-components'
 
 const GlobalStyle = createGlobalStyle`
-  /* http://meyerweb.com/eric/tools/css/reset/
-    v2.0 | 20110126
-    License: none (public domain)
-  */
+  ${({ theme }) => css`
+    * {
+      margin: 0;
+      padding: 0;
+      box-sizing: border-box;
+      -webkit-font-smoothing: antialiased;
+      -moz-osx-font-smoothing: grayscale;
 
-  * {
-    box-sizing: border-box;
-  }
+      ::-moz-selection {
+        color: ${theme.colors.white};
+        background: ${theme.colors.primary};
+      }
 
-  html, body, div, span, applet, object, iframe,
-  h1, h2, h3, h4, h5, h6, p, blockquote, pre,
-  a, abbr, acronym, address, big, cite, code,
-  del, dfn, em, img, ins, kbd, q, s, samp,
-  small, strike, strong, sub, sup, tt, var,
-  b, u, i, center,
-  dl, dt, dd, ol, ul, li,
-  fieldset, form, label, legend,
-  table, caption, tbody, tfoot, thead, tr, th, td,
-  article, aside, canvas, details, embed,
-  figure, figcaption, footer, header, hgroup,
-  menu, nav, output, ruby, section, summary,
-  time, mark, audio, video {
-    margin: 0;
-    padding: 0;
-    border: 0;
-    font-size: 100%;
-    font: inherit;
-    vertical-align: baseline;
-    text-decoration: none;
-    outline: none;
-  }
+      ::selection {
+        color: ${theme.colors.white};
+        background: ${theme.colors.primary};
+      }
+    }
 
-  /* HTML5 display-role reset for older browsers */
-  article, aside, details, figcaption, figure,
-  footer, header, hgroup, menu, nav, section {
-    display: block;
-  }
+    html {
+      font-size: 62.5%;
+    }
 
-  body {
-    line-height: 1;
-  }
+    body,
+    button,
+    input {
+      font-family: 'Poppins', sans-serif;
+      font-size: 2rem;
+      font-weight: 400;
+      line-height: 1;
+    }
 
-  ol, ul {
-    list-style: none;
-  }
+    *,
+    ::before,
+    ::after {
+      border-width: 0;
+      border-style: solid;
+      border-color: currentColor;
+    }
 
-  blockquote, q {
-    quotes: none;
-  }
+    blockquote,
+    dl,
+    dd,
+    h1,
+    h2,
+    h3,
+    h4,
+    h5,
+    h6,
+    hr,
+    figure,
+    p,
+    pre {
+      margin: 0;
+    }
 
-  blockquote:before, blockquote:after,
-  q:before, q:after {
-    content: '';
-    content: none;
-  }
+    h1,
+    h2,
+    h3,
+    h4,
+    h5,
+    h6 {
+      font-size: inherit;
+      font-weight: inherit;
+    }
 
-  table {
-    border-collapse: collapse;
-    border-spacing: 0;
-  }
+    ol,
+    ul {
+      list-style: none;
+      margin: 0;
+      padding: 0;
+    }
 
-  /*PROJECT RULES*/
+    img,
+    svg,
+    video,
+    canvas,
+    audio,
+    iframe,
+    embed,
+    object {
+      display: block;
+    }
 
-  * {
-    font-family: 'Poppins', sans-serif;
-  }
+    button {
+      cursor: pointer;
+    }
 
-  html {
-    font-size: 62.5%;
-  }
+    button:focus {
+      outline: 1px dotted;
+      outline: 5px auto -webkit-focus-ring-color;
+    }
 
-  body,
-  button,
-  input {
-    font-family: 'Poppins', sans-serif;
-    font-size: 1.4rem;
-    font-weight: 400;
-  }
+    a {
+      text-decoration: none;
+    }
+  `}
 `
 export default GlobalStyle
