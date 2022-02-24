3966a9e72ed0a202dce116d8623b29ab26fc5d78
Luís Grizzo <luis.grizzo@liveonsolutions.com>
2022-02-24 13:36:19 -0300

    feat(MainLayout): add layout final version

--- src/layouts/MainLayout/index.tsx
+++ src/layouts/MainLayout/index.tsx
@@ -1,20 +1,13 @@
 import { useState } from 'react'
 import { Link } from 'react-router-dom'
 import { useSpring, animated } from 'react-spring'
-import IndianaDrag from 'react-indiana-drag-scroll'
 import { FaLinkedinIn, FaGithub, FaInstagram } from 'react-icons/fa'
 import { MdMenu } from 'react-icons/md'
 
-import Logo from '../../components/Logo'
-import Menu from '../../components/Menu'
+import Logo from 'components/Logo'
+import Menu from 'components/Menu'
 
 import * as S from './styles'
-
-import html5 from '../../assets/html5.png'
-import css3 from '../../assets/css3.png'
-import js from '../../assets/javascript.png'
-import ts from '../../assets/typescript.png'
-import react from '../../assets/react.png'
 
 export interface MainLayoutProps {
   children: React.ReactNode
@@ -23,50 +16,57 @@ export interface MainLayoutProps {
 const MainLayout = ({ children }: MainLayoutProps): React.ReactElement => {
   const [menuIsOpen, setMenuIsOpen] = useState(false)
 
-  const navbarStyle = useSpring({ from: { opacity: 0, y: -500 }, to: { opacity: 1, y: 0 } })
-
-  const asideStyle = useSpring({ from: { opacity: 0, x: 500 }, to: { opacity: 1, x: 0 } })
+  const navbarAnimation = useSpring({
+    from: {
+      y: -300
+    },
+    to: {
+      y: 0
+    }
+  })
 
   return (
     <S.Wrapper>
-      <animated.nav style={navbarStyle} className='w__navbar'>
-        <Link to="/" >
+      <animated.nav style={navbarAnimation} className="w__navbar">
+        <Link to="/">
           <Logo />
         </Link>
         <div className="wn__link-wrapper">
-          <a href="https://www.linkedin.com/in/lu%C3%ADs-ot%C3%A1vio-gaido-grizzo-2a957a1b2/" target='_blank' className="wnlw__link">
-            <FaLinkedinIn size={30}/>
+          <a
+            href="https://www.linkedin.com/in/lu%C3%ADs-ot%C3%A1vio-gaido-grizzo-2a957a1b2/"
+            target="_blank"
+            className="wnlw__link"
+            rel="noreferrer"
+          >
+            <FaLinkedinIn size={30} />
           </a>
-          <a href="https://github.com/luis-grizzo" target='_blank' className="wnlw__link">
-            <FaGithub size={30}/>
+          <a
+            href="https://github.com/luis-grizzo"
+            target="_blank"
+            className="wnlw__link"
+            rel="noreferrer"
+          >
+            <FaGithub size={30} />
           </a>
-          <a href="https://www.instagram.com/luis_ozzirg/" target='_blank' className="wnlw__link">
-            <FaInstagram size={30}/>
+          <a
+            href="https://www.instagram.com/luis_ozzirg/"
+            target="_blank"
+            className="wnlw__link"
+            rel="noreferrer"
+          >
+            <FaInstagram size={30} />
           </a>
-          <span className="wnl__divider" />
-          <button type='button' onClick={() => setMenuIsOpen(prevState => !prevState)} className='wnl__menu-button'>
-            <MdMenu size={30} className='wnclmb__icon' />
+          <span className="wnlw__divider" />
+          <button
+            type="button"
+            onClick={() => setMenuIsOpen((prevState) => !prevState)}
+            className="wnlw__menu-button"
+          >
+            <MdMenu size={30} className="wnclmb__icon" />
           </button>
         </div>
       </animated.nav>
-      <main className='w__main'>
-        <IndianaDrag
-          horizontal={false}
-          className='wm__page'
-        >
-          {children}
-        </IndianaDrag>
-        <animated.aside style={asideStyle} className="wm__aside">
-          <div className="wma__techs-wrapper">
-            <img src={html5} alt="html 5" className='wmatw__tech' />
-            <img src={css3} alt="css 3" className='wmatw__tech' />
-            <img src={js} alt="javascript" className='wmatw__tech' />
-            <img src={ts} alt="typescript" className='wmatw__tech' />
-            <img src={react} alt="react" className='wmatw__tech' />
-          </div>
-          <div className="wma__portrait" />
-        </animated.aside>
-      </main>
+      <main className="w__main">{children}</main>
       <Menu isOpen={menuIsOpen} onClose={() => setMenuIsOpen(false)} />
     </S.Wrapper>
   )
--- src/layouts/MainLayout/styles.ts
+++ src/layouts/MainLayout/styles.ts
@@ -1,40 +1,34 @@
 import styled, { css } from 'styled-components'
 
-import portrait from '../../assets/portrait.png'
-
 export const Wrapper = styled.div`
   ${({ theme }) => css`
     display: flex;
     flex-direction: column;
-    gap: 5rem;
 
     height: 100vh;
     width: 90vw;
     max-width: 128rem;
     margin: 0 auto;
-    padding: 5rem 0;
     overflow: hidden;
 
     @media screen and (max-width: ${`${theme.mediaquerys.laptopStart}px`}) {
-      gap: 3rem;
-
-      height: auto;
-      padding: 3rem 0;
-      overflow: auto;
+      height: unset;
+      min-height: 100vh;
     }
 
     .w__navbar {
       display: flex;
       align-items: center;
-      gap: 4rem;
+      gap: 5rem;
 
       width: 100%;
+      padding: 2.5rem 0;
 
       .wn__link-wrapper {
         display: flex;
         align-items: center;
         justify-content: flex-end;
-        gap: 2rem;
+        gap: 2.5rem;
 
         width: 100%;
 
@@ -46,20 +40,20 @@ export const Wrapper = styled.div`
           color: ${theme.colors.black};
 
           will-change: color;
-          transition: ${theme.transitions.default};
+          transition: ${`color ${theme.transitions.new}`};
 
           &:hover {
             color: ${theme.colors.blackHover};
           }
         }
 
-        .wnl__divider {
-          width: .2rem;
+        .wnlw__divider {
+          width: 0.2rem;
           height: 3rem;
           background-color: ${theme.colors.black};
         }
 
-        .wnl__menu-button {
+        .wnlw__menu-button {
           display: flex;
           align-items: center;
           justify-content: center;
@@ -71,7 +65,7 @@ export const Wrapper = styled.div`
           padding: 0;
 
           will-change: color;
-          transition: ${theme.transitions.default};
+          transition: ${`color ${theme.transitions.new}`};
 
           &:hover {
             color: ${theme.colors.blackHover};
@@ -81,122 +75,27 @@ export const Wrapper = styled.div`
     }
 
     .w__main {
-      display: flex;
+      display: grid;
+      grid-template-columns: calc(40% - 2.5rem) calc(60% - 2.5rem);
       align-items: center;
       gap: 5rem;
 
       height: 100%;
       width: 100%;
+      padding: 2.5rem 0 14.5rem;
       overflow: hidden;
 
       @media screen and (max-width: ${`${theme.mediaquerys.laptopStart}px`}) {
-        flex-direction: column-reverse;
-        justify-content: flex-end;
+        grid-template-columns: 1fr;
 
-        overflow: visible;
-      }
+        padding: 2.5rem 0;
 
-      .wm__page {
-        display: flex;
-        align-items: center;
-
-        width: 60%;
-        height: 100%;
-        overflow: auto;
+        [class*='title'] {
+          font-size: 5.8rem;
+        }
 
         [class*='description'] {
           line-height: normal;
-        }
-
-        @media screen and (max-width: ${`${theme.mediaquerys.laptopStart}px`}) {
-          height: auto;
-          width: 100%;
-          overflow: visible;
-
-          [class*='title'] {
-            font-size: 5.8rem;
-          }
-        }
-
-        ::-webkit-scrollbar {
-          opacity: 0;
-          width: 8px;
-        }
-
-
-        &:hover {
-          ::-webkit-scrollbar {
-            opacity: 1;
-          }
-
-          ::-webkit-scrollbar-track {
-            opacity: 0;
-          }
-
-          ::-webkit-scrollbar-thumb {
-            background-color: ${theme.colors.text};
-            border-radius: 1rem;
-
-            &:hover {
-              background-color: ${theme.colors.textHover};
-            }
-          }
-        }
-
-      }
-
-      .wm__aside {
-        display: flex;
-        align-items: flex-end;
-        gap: 1rem;
-
-        height: 100%;
-        max-height: 60rem;
-        width: 40%;
-
-        @media screen and (max-width: ${`${theme.mediaquerys.laptopStart}px`}) {
-          width: 100%;
-        }
-
-        .wma__techs-wrapper {
-          display: flex;
-          flex-direction: column;
-          align-items: center;
-          gap: 1rem;
-
-          .wmatw__tech {
-            width: 5rem;
-          }
-        }
-
-        .wma__portrait {
-          display: flex;
-          align-items: flex-end;
-
-          height: 100%;
-          width: 100%;
-
-          background-image: url(${portrait});
-          background-size: cover;
-          background-position-x: 45%;
-          background-repeat: no-repeat;
-
-          border-radius: 50px;
-          border-bottom-left-radius: 10px;
-          overflow: hidden;
-
-          @media screen and (max-width: ${`${theme.mediaquerys.laptopStart}px`}) {
-            height: 45rem;
-          }
-
-          &::before {
-            content: '';
-            width: 100%;
-            height: calc(100% - 8rem);
-            background-color: ${theme.colors.primary};
-            z-index: -1;
-            border-radius: inherit;
-          }
         }
       }
     }
