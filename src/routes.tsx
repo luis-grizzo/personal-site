f7f7741442c3bc21b79e14485cd47ebedca51b79
Luís <luisoggrizzo@gmail.com>
2022-09-05 23:58:05 -0300

    feat: add ne wportfolio page route

--- src/routes.tsx
+++ src/routes.tsx
@@ -10,6 +10,7 @@ const MainLayout = lazy(async () => await import('./layouts/MainLayout'))
 const Home = lazy(async () => await import('./pages/Home'))
 const About = lazy(async () => await import('./pages/About'))
 const Contact = lazy(async () => await import('./pages/Contact'))
+const Portifolio = lazy(async () => await import('./pages/Portfolio'))
 
 const Router = (): React.ReactElement => {
   return (
@@ -21,6 +22,7 @@ const Router = (): React.ReactElement => {
               <Route index element={<Home />} />
               <Route path="/about" element={<About />} />
               <Route path="/contact" element={<Contact />} />
+              <Route path="/portfolio" element={<Portifolio />} />
             </Routes>
           </MainLayout>
         </ActivePageProvider>
